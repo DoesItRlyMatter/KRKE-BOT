8b7b49b4d633351077adef3c91c5eabe75e69674
dirm <anton.kerke@gmail.com>
2021-04-02 00:51:01 +0300

    initial commit

---
+++ KRKE-BOT.py
@@ -0,0 +1,148 @@
+# NOTES FOR USAGE
+# FILL IN THE FOLLOWING:
+#   - TOKEN
+#   - teamspeak ...?address=YOUR TEAMSPEAK ADDRESS HERE
+#   - bitwarden ...get('yourdomainhere')
+#   - ubooquity ...get('yourdomainhere')
+#   - plex ...get('yourdomainhere')
+
+import discord
+from discord.ext import commands
+from discord import Intents
+import requests
+import time
+
+BOT_NAME = 'KRKE'
+TOKEN = ''
+START_TIME = time.time()
+
+client = commands.Bot(command_prefix='.', intents=Intents.all())
+
+# call ts api, return true/false.
+
+
+def server_status():
+    status = dict()
+    # TS
+    teamspeak = requests.get('https://api.cleanvoice.ru/ts3/?address=')
+    teamspeak = teamspeak.json()
+    teamspeak = teamspeak['can_connect']
+    if teamspeak is True:
+        status.update({"ts": "+ Teamspeak3  - OK!"})
+    else:
+        status.update({"ts": "- Teamspeak3  - F"})
+    # NEXTCLOUD
+    nextcloud = requests.get('https://nextcloud')
+    if nextcloud.status_code == 200:
+        status.update({"nextcloud": "+ Nextcloud   - OK!"})
+    else:
+        status.update({"nextcloud": "- NextCloud   - F"})
+    # BITWARDEN
+    bitwarden = requests.get('https://bitwarden')
+    if bitwarden.status_code == 200:
+        status.update({"bitwarden": "+ Bitwarden   - OK!"})
+    else:
+        status.update({"bitwarden": "+ Bitwarden   - F"})
+    # UBOOQUITY
+    ubooquity = requests.get('https://books')
+    if ubooquity.status_code == 200:
+        status.update({"books": "+ Ubooquity   - OK!"})
+    else:
+        status.update({"books": "- Ubooquity   - F"})
+    # PLEX
+    plex = requests.get('https://plex')
+    if plex.status_code == 200:
+        status.update({"plex": "+ Plex        - OK!"})
+    else:
+        status.update({"plex": "- Plex        - F"})
+    return status
+
+
+# seconds split into days, hours, minutes and seconds.
+def split_seconds(seconds):
+    # Dict for storing days, hours, minutes and seconds.
+    split_time = {
+        'days': 0,
+        'hours': 0,
+        'minutes': 0,
+        'seconds': 0
+    }
+
+    # Convert seconds to days, hours, minutes and seconds.
+    days = seconds // (24 * 3600)
+    split_time.update({'days': int(days)})
+    seconds = seconds % (24 * 3600)
+    hours = seconds // 3600
+    split_time.update({'hours': int(hours)})
+    seconds %= 3600
+    minutes = seconds // 60
+    split_time.update({'minutes': int(minutes)})
+    seconds %= 60
+    seconds = seconds
+    split_time.update({'seconds': int(seconds)})
+    # Return dict with values.
+    return split_time
+
+
+# Convert split_time dict to string (String xD xH xM xS)
+def complile_string(time_dict, start_string):
+    if time_dict['days'] != 0:
+        start_string += str(time_dict['days']) + 'd '
+    if time_dict['hours'] != 0:
+        start_string += str(time_dict['hours']) + 'h '
+    if time_dict['minutes'] != 0:
+        start_string += str(time_dict['minutes']) + 'm '
+    if time_dict['seconds'] != 0:
+        start_string += str(time_dict['seconds']) + 's '
+    # Return compiled string.
+    return start_string
+
+
+# function that returns bot uptime.
+def bot_uptime():
+    # Calculate time bot has been running.
+    time_since_start_seconds = time.time() - START_TIME
+    # Convert seconds to readable format.
+    uptime = complile_string(split_seconds(time_since_start_seconds), 'Bot has been running for ')
+    # Return string.
+    return uptime
+
+
+# Get bot avatar.
+# with open('bot_avatar.jpg', 'rb') as f:
+    # image = f.read()
+
+
+@client.event
+async def on_connect():
+    print('Bot connected to discord.')
+
+
+@client.event
+async def on_ready():
+    print('Bot ready!')
+    # assign bot avatar and name
+    # await client.user.edit(avatar=image)
+    await client.user.edit(username=BOT_NAME)
+
+
+@client.command(aliases=['s', 'status'])
+async def server(ctx):
+    status = server_status()
+    await ctx.send('```diff\n' + status["nextcloud"] + '\n' + status["bitwarden"] + '\n' + status["books"] + '\n' + status["ts"] + '\n' +
+                   status["plex"] + '```')
+
+
+@client.command(aliases=['upt'])
+async def uptime(ctx):
+    await ctx.send('```diff\n' + bot_uptime() + '```')
+
+
+@client.event
+async def on_member_join(member):
+    print('Someone joined the server.')
+    role = discord.utils.get(member.guild.roles, name='Guest')
+    await member.add_roles(role, reason='Autoassign-guest')
+
+
+client.run(TOKEN)
